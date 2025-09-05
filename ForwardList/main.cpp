75a5856af6fd973d5ac23742f716277bba9cb204
Admin <Admin@DESKTOP-8URQBFL>
2025-09-05 23:59:26 +0500

    Operator + Done

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -1,8 +1,10 @@
 ﻿//ForwardList
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 #define tab  "\t"
+#define delimiter "\n-------------------------------------------------------------\n"
 
 class Element
 {
@@ -30,6 +32,7 @@ public:
 
 	}
 	friend class ForwardList;
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
 
 int Element::count = 0;
@@ -39,6 +42,15 @@ class ForwardList
 	Element* Head;
 	size_t size;
 public:
+	Element* get_Head() const
+	{
+		return Head;
+	}
+	size_t get_size() const
+	{
+		return size;
+	}
+
 	ForwardList()
 	{
 		Head = nullptr;
@@ -47,12 +59,30 @@ public:
 		//если список пуст, то его Голова обязательно указывает на 0;
 		cout << "FLConstructor:\t" << this << endl;
 	}
-
+	ForwardList(const ForwardList& other) : ForwardList()
+	{
+		//Deep copy (побитовое копирование)
+		*this = other;
+		cout << "FLCopyConstructor:\t" << this << endl;
+	}
 	~ForwardList()
 	{
-		while (Head) pop_back();
+		clock_t t_start = clock();
+		while (Head) pop_front();
+		clock_t t_end = clock();
+		cout << "FLDestructor:\t" << this << "\tcomplete for " << double(t_end - t_start) / CLOCKS_PER_SEC << " sec. " << endl;
+	}
 
-		cout << "FLDestructor:\t" << this << endl;
+	//      Operators
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other) return *this; //0) Проверить, что This и Other разные объекты
+		while (Head) pop_front();         //1) Очистить данные
+		//2)Deep copy (побитовое копирование)
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "FLCopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	//		Adding elements
@@ -138,11 +168,22 @@ public:
 		cout << "Количество элементов списка: " << size << endl;
 		cout << "Общее количество элементов: " << Element::count << endl;
 	}
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
 
+ForwardList operator+(const ForwardList& left, const ForwardList& right)
+{
+	ForwardList fusion;
+	for (Element* Temp = left.get_Head(); Temp; Temp = Temp->pNext)
+		fusion.push_back(Temp->Data);
+	for (Element* Temp = right.Head; Temp; Temp = Temp->pNext)
+		fusion.push_back(Temp->Data);
+	return fusion;
+}
+
 //#define BASE_CHECK
-//#define COUNT_CHECK
-#define PERFORMANCE_CHECK
+#define OPERATOR_PLUS_CHECK
+//#define PERFORMANCE_CHECK
 
 
 void main()
@@ -170,7 +211,7 @@ void main()
 	list.print();*/
 #endif // BASE_CHECK
 
-#ifdef COUNT_CHECK
+#ifdef OPERATOR_PLUS_CHECK
 	ForwardList list1;
 	list1.push_back(0);
 	list1.push_back(1);
@@ -189,26 +230,35 @@ void main()
 	list2.push_back(89);
 	list2.print();
 
-	int Index;
+	/*int Index;
 	int value;
 	cout << "Введите индекс добавляемого элемента: "; cin >> Index;
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
-	list1.insert(value, Index);
 	list1.print();
 	list2.print();
-#endif // COUNT_CHECK
+	list1.insert(value, Index);*/
+
+	//ForwardList fusion = list1 + list2;  //CopyConstructor
+	ForwardList fusion;
+	cout << delimiter << endl;
+	fusion = list1 + list2;   //CopyAssignment
+	cout << delimiter << endl;
+	fusion.print();
+#endif // OPERATOR_PLUS_CHECK
 
 #ifdef PERFORMANCE_CHECK
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 	ForwardList list;
+	clock_t t_start = clock();
 	for (int i = 0; i < n; i++)
 	{
-		list.push_back(rand() % 100);
-		//list.push_front(rand() % 100);
+		//list.push_back(rand() % 100);
+		list.push_front(rand() % 100);
 	}
+	clock_t t_end = clock();
 
-	cout << "ForwardList filled. ";
+	cout << "ForwardList filled. " << double(t_end - t_start)/CLOCKS_PER_SEC << " sec. ";
 	system("PAUSE");
 #endif // PERFORMANCE_CHECK
 
